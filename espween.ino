a3e90a1ab63c681d2443184b0a19f29b32b7c31f
Martin Donlon <github-martin@donlons.com>
2016-10-29 15:31:00 +0000

    ESP based stranger things wall

---
+++ espween.ino
@@ -0,0 +1,170 @@
+#include <WiFiClientSecure.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiSTA.h>
+#include <WiFiUdp.h>
+#include <WiFiServer.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiScan.h>
+
+#include <DNSServer.h>
+
+#include <FastLED.h>
+#include <ESP8266WebServer.h>
+
+#define NUM_LEDS 50
+#define NUM_LETTERS 26
+#define NUM_COLORS 5
+
+const int letterLedIndex[NUM_LETTERS] = { 46, 45, 44, 42, 41, 40, 39, 37, 19, 21, 22, 23, 24, 25, 27, 28, 31, 14, 12, 10, 9, 8, 6, 5, 4, 1 };
+const CRGB letterColors[NUM_COLORS] = { CRGB::Red, CRGB::Yellow, CRGB::Green, CRGB::Blue, CRGB::Purple };
+
+struct Display {
+  CRGB m_leds[NUM_LEDS];
+  int m_nextCharacterIndex;
+  char m_message[128];
+  unsigned long m_nextUpdate;
+  
+  Display() { }
+
+  void setup() {
+    m_nextUpdate = 0;
+    m_nextCharacterIndex = 0;
+    FastLED.addLeds<WS2812, 0>(m_leds, NUM_LEDS);
+    setMessage( "Stranger Things" );
+  }
+
+  void setMessage( const String &str ) {
+    char *out = m_message;
+    const char *endOut = m_message + sizeof( m_message ) - 1;
+    const char *in = str.c_str();
+    while( *in && out < endOut )
+    {
+      char c = toupper( *in );
+      if( ( c >= 'A' && c <= 'Z' ) || c == ' ' ) {
+        *out = c;
+        out++;
+      }
+      in++;
+    }
+    *out = '\0';
+  
+    Serial.printf( "Message changed: %s", m_message );
+    Serial.println();
+
+    m_nextCharacterIndex = 0;
+    m_nextUpdate = millis() + 1000;
+  }
+
+  const char *getMessage() {
+    return m_message;
+  }
+
+  void clearAll() {
+    for( int i = 0; i < NUM_LEDS; i++ )
+    {
+      m_leds[i] = CRGB::Black;
+    }
+  }
+  
+  void update() {
+    unsigned long ms = millis();
+
+    if( ms < m_nextUpdate ) {
+      return;
+    }
+
+    clearAll();
+
+    int len = strlen( m_message );
+    if( m_nextCharacterIndex >= len ) {
+      m_nextUpdate = ms + 3000;
+      m_nextCharacterIndex = 0;
+    } else {
+      char c = m_message[m_nextCharacterIndex];
+      if( c >= 'A' && c <= 'Z' ) {
+        int idx = letterLedIndex[ c - 'A' ];
+        m_leds[idx] = letterColors[ idx % NUM_COLORS ];
+        m_nextUpdate = ms + 600;
+      } else {
+        m_nextUpdate = ms + 400;
+      }
+      
+      m_nextCharacterIndex++;
+    }
+    FastLED.show();
+  }
+};
+
+
+ESP8266WebServer server(80);
+Display display;
+const byte DNS_PORT = 53;
+DNSServer dnsServer;
+
+
+void handleUpdate() {
+  String msg = server.arg( "message" );
+  display.setMessage( msg );
+
+  server.sendHeader( "Location", "http://stranger.things/" );
+  server.send( 301 );
+}
+
+void handleRoot() {
+  char output[1024];
+  sprintf( output, "\
+<!doctype html>\
+<html lang='en'>\
+<head>\
+  <meta charset='utf-8'>\
+  <meta name='viewport' content='width=device-width'>\
+  <title>Strange Things</title>\
+</head>\
+<body>\
+  <h2>Current: %s</h2>\
+  <form method='POST' action='/update'>\
+    <input type='text' name='message'>\
+    <input type='submit' value='Update'>\
+  </form>\
+</body>\
+</html>", display.getMessage() );
+
+  server.send(200, "text/html", output);
+}
+
+void setup() {
+  delay(1000);
+  
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Configuring access point...");
+  WiFi.softAP("StrangerThings");
+  
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+
+  dnsServer.start(DNS_PORT, "*", myIP);
+
+  server.on("/", handleRoot);
+  server.on("/update", handleUpdate);
+  server.onNotFound( []() {
+    server.sendHeader( "Location", "http://stranger.things/" );
+    server.send( 301 );
+  } );
+  
+  server.begin();
+  Serial.println("HTTP server started");
+
+  display.setup();
+}
+
+void loop() {
+  server.handleClient();
+  dnsServer.processNextRequest();
+  display.update();
+}
